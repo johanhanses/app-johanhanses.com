82edaba52b06fe27d1ecd61e5b1c475066149e7a
Johan Hanses <johanhanses@gmail.com>
2023-01-14 20:06:07 +0100

    prettier

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ One more time...
 #TODO
 
 - ~Decide which icon lib to use.~
-- Tailwind-Prettier or eslint from antfu?
+- ~Tailwind-Prettier or eslint from antfu?~
 - Make next auth work in appDir.
 - Make color mode work in appDir.
 - Prisma.
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
 import { Noto_Serif_Display } from '@next/font/google'
 import './globals.css'
 
-const notoSerifDisplay = Noto_Serif_Display({ variable: '--noto-serif-display', subsets: ['latin'] })
+const notoSerifDisplay = Noto_Serif_Display({
+  variable: '--noto-serif-display',
+  subsets: ['latin']
+})
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head />
-      <body className={`min-h-screen ${notoSerifDisplay.variable} font-notoSerifDisplay px-6 antialiased`}>
+      <body className={`min-h-screen ${notoSerifDisplay.variable} px-6 font-notoSerifDisplay antialiased`}>
         {children}
       </body>
     </html>
