a8dd4785bd1b489313aa074ec450109637bd7b56
Johan Hanses <johanhanses@gmail.com>
2023-01-15 10:04:32 +0100

    Added Prisma and connected to PlanetScale

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { ThemeToggler } from '@/components/ThemeToggler'
 
-export default function Home() {
+export default async function Home() {
   return (
     <div>
       <h1 className="text-2xl font-bold">JH</h1>
