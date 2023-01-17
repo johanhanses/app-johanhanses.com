f493134242232c405d83871b1846ec2cc6ee284e
Johan Hanses <johanhanses@gmail.com>
2023-01-17 20:42:19 +0100

    Animate header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useBoundedScroll } from '@/hooks/useBoundedScroll'
+import { motion, useTransform } from 'framer-motion'
 import { Menu } from './Menu'
 import { MobileMenu } from './MobileMenu'
 import { ThemeToggler } from './ThemeToggler'
@@ -27,15 +31,30 @@ const menuItems: MenuItems[] = [
 ]
 
 export const Header = () => {
+  const { scrollYBoundedProgress } = useBoundedScroll(200)
+  const scrollYBoundedProgressThrottled = useTransform(
+    scrollYBoundedProgress,
+    [0, 0.5, 1],
+    [0, 0, 1]
+  )
+
   return (
-    <header className="relative flex h-24 w-full items-center justify-end gap-4 px-4 sm:px-8 md:justify-center lg:px-12">
-      <Menu menuItems={menuItems} />
+    <div className="fixed flex w-full max-w-7xl">
+      <motion.header
+        style={{
+          height: useTransform(scrollYBoundedProgressThrottled, [0, 1], [96, 0]),
+          opacity: useTransform(scrollYBoundedProgressThrottled, [0, 1], [1, 0])
+        }}
+        className="relative flex w-full items-center justify-end gap-4 overflow-hidden px-4 sm:px-8 md:justify-center lg:px-12"
+      >
+        <Menu menuItems={menuItems} />
 
-      <MobileMenu menuItems={menuItems} />
+        <MobileMenu menuItems={menuItems} />
 
-      {/* <AuthButton /> */}
-      <ThemeToggler />
-    </header>
+        {/* <AuthButton /> */}
+        <ThemeToggler />
+      </motion.header>
+    </div>
   )
 }
 2
