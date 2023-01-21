b52ad97b48a8d3192e5a3b97116d82b73b8c88dd
Johan Hanses <johanhanses@gmail.com>
2023-01-21 19:37:41 +0100

    remove period

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -41,7 +41,7 @@ export default function About() {
           <p>
             Another thing that will be available is small demos of various animated
             elements/features, mainly using the{' '}
-            <ALink href="https://www.framer.com/motion/">Framer Motion.</ALink> library.
+            <ALink href="https://www.framer.com/motion/">Framer Motion</ALink> library.
           </p>
           <p>This text and the whole app will be constantly changing.</p>
         </TextBlock>
